ed1f04c01fda5801db54a79488191dbd664f4797
Usuario <Usuario@192.168.1.136>
2025-05-11 14:03:10 +0200

    Para Marina

---
+++ src/Repaso/Repaso_Dani.java
@@ -0,0 +1,27 @@
+package Repaso;
+
+public class Repaso_Dani {
+
+	public static void main(String[] args) {
+		System.out.println("🌟✨ CADENA MÁGICA DE BUENA SUERTE ✨🌟\\r\\n\"\r\n"
+				+ "				+ \"\\r\\n\"\r\n"
+				+ "				+ \"Hola! 👋 Esta no es una cadena cualquiera… ¡es una cadena mágica! 🪄\\r\\n\"\r\n"
+				+ "				+ \"\\r\\n\"\r\n"
+				+ "				+ \"📅 Si estás leyendo esto, vas a tener una semana llena de:\\r\\n\"\r\n"
+				+ "				+ \"\\r\\n\"\r\n"
+				+ "				+ \"✅ Buenas noticias\\r\\n\"\r\n"
+				+ "				+ \"✅ Risas inesperadas\\r\\n\"\r\n"
+				+ "				+ \"✅ Un dinerito extra 💸\\r\\n\"\r\n"
+				+ "				+ \"✅ Y mucha, MUCHA buena vibra ✨\\r\\n\"\r\n"
+				+ "				+ \"\\r\\n\"\r\n"
+				+ "				+ \"Pero solo si la compartes con 5 personas antes de que pasen 5 minutos… ⏰\\r\\n\"\r\n"
+				+ "				+ \"\\r\\n\"\r\n"
+				+ "				+ \"No lo rompas 😱 o te quedarás sin memes buenos esta semana 😂\\r\\n\"\r\n"
+				+ "				+ \"\\r\\n\"\r\n"
+				+ "				+ \"¿Aceptas el reto? 🔁");
+	}
+
+
+
+
+}
