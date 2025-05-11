cddbacf5bc6eadf16a68b485dfb7a81a0cb730ca
Marina Torre Duran <Marina Torre Duran@LAPTOP-7AN9ONCM>
2025-05-11 15:30:13 +0200

    Para Dani

---
+++ src/Repaso/DeMarina.java
@@ -0,0 +1,14 @@
+package Repaso;
+
+public class DeMarina {
+
+	public static void main(String[] args) {
+		
+		System.out.println("✨¡Cadena de buena vibra!✨\n"
+				+ " Recibirás un cumplido anónimo.\n"
+				+ "Pasa esta cadena a 5 personas que te caigan bien.\n"
+				+ "¡Alegremos el día! 😊");
+
+	}
+
+}
